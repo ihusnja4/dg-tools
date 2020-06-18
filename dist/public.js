846d5c521be8a5f98e07830af0594cd5f2b083f3
Ivan Hušnjak <ihusnja4@gmail.com>
2020-06-18 16:26:45 +0200

    Integrated rollup bundler

---
+++ dist/public.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    /**
+     * @description Public tools for DarkGalaxy game @link <https://www.darkgalaxy.com/>
+     *
+     * @author Ivan (aka Deda)
+     * @version 0.1
+     */
+    (function(env) {
+        
+        /**
+         * @namespace DGTOOLSv1
+         */
+        env.DGTOOLSv1 = {
+            connect: connect
+        };
+        
+        // region View scripts
+        
+        function connect(options = {}) {
+            // TODO determine which DarkGalaxy view script is running in
+        }
+        
+        // endregion
+        
+    })(window);
+
+}());
